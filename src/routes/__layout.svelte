ba8ac2dc582cbea7ec721c72d51da507d1ee8a9b
Gustavo Zimbron <pptavozl@gmail.com>
2022-03-01 21:46:45 -0600

    Scroll to element in page in svelte/sveltekit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,40 @@
+<header>
+  <h1>
+    ZimbronApps.com
+    <span class="componente"> Scroll to Element </span>
+  </h1>
+  <div class="svelte">SvelteKit</div>
+</header>
+<slot />
+
+<style>
+  header {
+    background: steelblue;
+    color: white;
+    text-align: center;
+    padding: 5vh 20px;
+    height: 150px;
+  }
+  h1 {
+    font-family: 'Yanone Kaffeesatz', sans-serif;
+    font-size: 2.5em;
+    text-align: 'center';
+    margin: 0;
+    display: inline-block;
+    letter-spacing: 1px;
+  }
+  h1 .componente {
+    color: skyblue;
+    font-size: 2rem;
+    letter-spacing: 2px;
+  }
+  .svelte {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    font-size: 1.1rem;
+    padding: 2px 5px;
+    background: lightblue;
+    color: steelblue;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { element } from 'svelte/internal';
+
+  const scrollToElement = (selector) => {
+    const elemento = document.querySelector(selector);
+    if (!elemento) return;
+
+    let posicion = elemento.getBoundingClientRect().top;
+    let offset = posicion + window.pageYOffset;
+
+    window.scrollTo({
+      top: offset,
+      behavior: 'smooth',
+    });
+  };
+</script>
+
+<ul>
+  <li>
+    <a href={'#'} on:click|preventDefault={() => scrollToElement('#seccion1')}
+      >Secc 1</a
+    >
+  </li>
+  <li>
+    <a href={'#'} on:click|preventDefault={() => scrollToElement('#seccion2')}
+      >Secc 2</a
+    >
+  </li>
+  <li>
+    <a href={'#'} on:click|preventDefault={() => scrollToElement('#seccion3')}
+      >Secc 3</a
+    >
+  </li>
+</ul>
+
+<div class="spacer" />
+<div class="spacer" />
+<div class="spacer" />
+<div id="seccion1"><h1>Seccion 1</h1></div>
+<div class="spacer" />
+<div class="spacer" />
+<div class="spacer" />
+<div id="seccion2"><h1>Seccion 2</h1></div>
+<div class="spacer" />
+<div class="spacer" />
+<div class="spacer" />
+<div id="seccion3"><h1>Seccion 3</h1></div>
+
+<style>
+  ul {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  li {
+    list-style: none;
+    padding: 0;
+    width: 100px;
+    height: 30px;
+  }
+
+  .spacer {
+    height: 500px;
+  }
+  h1 {
+    background: grey;
+    color: white;
+  }
+</style>
